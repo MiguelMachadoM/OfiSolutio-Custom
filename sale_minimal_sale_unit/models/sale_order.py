d63350bd2e7a68f65d75419dfa662cd20efd9414
Miguel Machado <memachado@gmail.com>
2017-11-04 11:48:15 +0100

    [10.0] Migration
    Migration to V10, Refeactored

--- sale_minimal_sale_unit/models/sale_order.py
+++ sale_minimal_sale_unit/models/sale_order.py
@@ -1,45 +1,44 @@
+
 # -*- encoding: utf-8 -*-
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from openerp import models, exceptions, fields, api
+from odoo import models, exceptions, fields, api
 
 
 class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
+        _inherit = "sale.order.line"
 
-    minimal_sale_unit = fields.Float(
-        string="UMV", related="product_id.minimal_sale_unit", readonly=True)
-    minimal_sale_special_unit = fields.Float(
-        string="UVE", related="product_id.minimal_sale_special_unit",
-        readonly=True)
+        minimal_sale_unit = fields.Float(
+                string="UMV", related="product_id.minimal_sale_unit", readonly=True)
+        minimal_sale_special_unit = fields.Float(
+                string="UVE", related="product_id.minimal_sale_special_unit",
+                readonly=True)
+        @api.multi
+        @api.onchange('product_uom_qty','product_id')
 
-    @api.multi
-    def product_id_change(
-            self, pricelist, product, qty=0, uom=False, qty_uos=0, uos=False,
-            name='', partner_id=False, lang=False, update_tax=True,
-            date_order=False, packaging=False, fiscal_position=False,
-            flag=False):
-        res = super(SaleOrderLine, self).product_id_change(
-            pricelist, product, qty=qty, uom=uom, qty_uos=qty_uos, uos=uos,
-            name=name, partner_id=partner_id, lang=lang, update_tax=update_tax,
-            date_order=date_order, packaging=packaging,
-            fiscal_position=fiscal_position, flag=flag)
-        if product:
-            qty = res['value'].get('product_uom_qty', qty)
-            product = self.env['product.product'].browse(product)
-            if product.minimal_sale_special_unit:
-                if qty < product.minimal_sale_special_unit:
-                    res['value']['product_uom_qty'] = (
-                        product.minimal_sale_special_unit)
-                if qty < product.minimal_sale_unit:
-                    # Apply increment and return
-                    res['value']['price_unit'] = res['value'].get(
-                        'price_unit', 0.0) * 1.10
-                    return res
-            if product.minimal_sale_unit:
-                multiple = int((qty - 0.0001) /
-                               product.minimal_sale_unit) + 1
-                res['value']['product_uom_qty'] = (multiple *
-                                                   product.minimal_sale_unit)
-        return res
+        def product_uom_change_qty(self):
+
+                if self.order_id.pricelist_id and self.order_id.partner_id:
+                        product = self.product_id.with_context(
+                        lang=self.order_id.partner_id.lang,
+                        partner=self.order_id.partner_id.id,
+                        quantity=self.product_uom_qty,
+                        date_order=self.order_id.date_order,
+                        pricelist=self.order_id.pricelist_id.id,
+                        uom=self.product_uom.id,
+                        fiscal_position=self.env.context.get('fiscal_position')
+                        )
+
+                        qty = self.product_uom_qty
+                        if product.minimal_sale_special_unit:
+                                if qty < product.minimal_sale_special_unit:
+                                        vals = {'product_uom_qty': (product.minimal_sale_special_unit)}
+                                if qty < product.minimal_sale_unit:
+                                # Apply increment and return
+                                        vals = {'price_unit': self.price_unit * 1.10}
+                                        self.update(vals)
+                        if product.minimal_sale_unit:
+                                multiple = int((qty - 0.0001) / product.minimal_sale_unit) + 1
+                                vals = {'product_uom_qty': (multiple * product.minimal_sale_unit)}
+                                self.update(vals)
